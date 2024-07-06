e5b0e94283a6ec69666b688d2cad6b598a1a5c4f
Hunter <muhammadhassanmughal47@gmail.com>
2024-07-07 02:40:29 +0500

    added responsive navbar

--- src/LandingPage/Navbar/Navbar.jsx
+++ src/LandingPage/Navbar/Navbar.jsx
@@ -1,19 +1,29 @@
 import React from 'react'
+import { useRef } from 'react';
+import { FaTimes, FaBars } from "react-icons/fa";
 import NavbarLogo from '../../Assets/den black 1.png' 
 import Search from '../../Assets/Search.png' 
 import './Navbar.css'
 
 const Navbar = () => {
+  const navRef = useRef();
+  const showNavbar = () => {
+    navRef.current.classList.toggle('responsive-navbar');
+  }
   return (
     <div className='navbar-container'>
+      <div className="logo-alignment">
+      <button onClick={showNavbar} className='nav-button'><FaBars className='icon'/></button>
       <div className="navbar-logo">
         <img src={NavbarLogo} alt=''/>
       </div>
-      <div className="navbar-items-container">
+      </div>
+      <div ref={navRef} className="navbar-items-container">
         <a href="/" className="navbar-item">Internships</a>
         <a href="/" className="navbar-item">Offers</a>
         <a href="/" className="navbar-item">Courses</a>
         <a href="/" className="navbar-item">Contact</a>
+        <button onClick={showNavbar} className='close-button'><FaTimes className='icon'/></button>
       </div>
       <div className="navbar-buttons">
         <img src={Search}  className='navbar-logo-search' alt="" />
